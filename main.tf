b0e74847eadd2ca07138fa7221e4b4db5bcd4859
deep-18 <deepraval7408@gmail.com>
2024-04-24 15:07:55 -0400

    Commiting with devops integration

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-04e5276ebb8451442"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.example.azureAgent
+
+  tags = {
+    Name = "example-instance"
+  }
+}
+
+resource "aws_security_group" "docker" {
+  name        = "docker_security_group"
+  description = "Allow inbound traffic on port 80"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "example" {
+  instance = aws_instance.example.id
+}
+
+resource "null_resource" "docker_deploy" {
+  depends_on = [aws_instance.example]
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install docker -y",
+      "sudo service docker start",
+      "sudo docker run -d -p 80:80 notification-app-image" 
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      private_key = file("./azureAgent.pem") 
+      host     = aws_instance.example.public_ip
+    }
+  }
+}
