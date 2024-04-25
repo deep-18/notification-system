bdf9461d3ad45bfcdb47fc36635984c441eeb95d
deep-18 <deepraval7408@gmail.com>
2024-04-25 12:03:04 -0400

    sd

--- main.tf
+++ main.tf
@@ -12,15 +12,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_key_pair" "example" {
-  key_name   = "azureAgent"
-  public_key = ${{ secrets.AZUREAGENT }}
-}
+
 
 resource "aws_instance" "example" {
   ami           = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.example.key_name
+  key_name      = "azureAgent"
 
   tags = {
     Name = "example-instance"
