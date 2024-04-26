37ea1e0d89878da19fdf421a52c6b0a355ace926
deep-18 <deepraval7408@gmail.com>
2024-04-26 11:28:32 -0400

    commit

--- main.tf
+++ main.tf
@@ -23,34 +23,17 @@ resource "aws_instance" "example" {
     Name = "example-instance"
   }
 
-  user_data = <<EOF
+  user_data = <<-EOF
+    #!/bin/bash
     sudo yum update -y
     sudo yum install docker -y
     sudo service docker start
     sudo docker login -u deepraval -p Deep12345
-    sudo docker pull deepraval/notification-system:90
-    sudo docker run -d -p 80:80 notification-system
+    sudo docker pull deepraval/notification-system:95
+    sudo docker run -d --name notification-system -p 80:8000 deepraval/notification-system:95
   EOF
 }
 
-resource "aws_security_group" "docker" {
-  name        = "docker_security_group"
-  description = "Allow inbound traffic on port 80"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 output "public_ip" {
   value = aws_instance.example.public_ip
