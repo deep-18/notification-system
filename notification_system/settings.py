59d7051939e4a164fba826fb120330c4c4676f0c
deep-18 <deepraval7408@gmail.com>
2024-04-26 11:15:46 -0400

    commit

--- notification_system/settings.py
+++ notification_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sb4d!i&mbre@eruuvdo9qp$dlb6#32ctji03ligoo7qa*s7h=9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
